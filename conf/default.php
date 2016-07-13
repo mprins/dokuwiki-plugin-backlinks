c1c5904b437371869be09187374c3dbd43b8511f
SC Yoo <dryoo@live.com>
2016-07-14 00:52:31 +0900

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -1,7 +1,2 @@
 <?php
-//$conf['watermark_path'] = '../tpl/dokuwiki/images/logo.png';
-//$conf['size']           = 10;
-//$conf['minsize']        = 300;
-//$conf['vertical']       = 80;
-//$conf['horizontal']     = 90;
 $conf['number']    = 20;
